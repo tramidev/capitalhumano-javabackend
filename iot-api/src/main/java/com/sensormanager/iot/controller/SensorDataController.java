2a6ebfcdbe9cf06ecae63139d6a7b8b3d06624a2
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2025-03-28 01:12:46 -0300

    CRUD User, Role, UserRole

--- iot-api/src/main/java/com/sensormanager/iot/controller/SensorDataController.java
+++ iot-api/src/main/java/com/sensormanager/iot/controller/SensorDataController.java
@@ -2,6 +2,7 @@ package com.sensormanager.iot.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,34 +22,40 @@ public class SensorDataController {
 
     private final SensorDataService sensorDataService;
 
+    @Autowired
     public SensorDataController(SensorDataService sensorDataService) {
         this.sensorDataService = sensorDataService;
     }
 
+    // Obtener todos los datos de sensores
     @GetMapping
     public ResponseEntity<List<SensorDataDTO>> getAllSensorData() {
         List<SensorDataDTO> sensorDataList = sensorDataService.getAllSensorData();
         return ResponseEntity.ok(sensorDataList);
     }
 
+    // Obtener datos de un sensor por ID
     @GetMapping("/{id}")
     public ResponseEntity<SensorDataDTO> getSensorDataById(@PathVariable Long id) {
         SensorDataDTO sensorDataDTO = sensorDataService.getSensorDataById(id);
         return (sensorDataDTO != null) ? ResponseEntity.ok(sensorDataDTO) : ResponseEntity.notFound().build();
     }
 
+    // Crear un nuevo registro de sensor
     @PostMapping
     public ResponseEntity<SensorDataDTO> createSensorData(@RequestBody SensorDataDTO sensorDataDTO) {
         SensorDataDTO createdSensorData = sensorDataService.createSensorData(sensorDataDTO);
         return ResponseEntity.ok(createdSensorData);
     }
 
+    // Actualizar datos de un sensor
     @PutMapping("/{id}")
     public ResponseEntity<SensorDataDTO> updateSensorData(@PathVariable Long id, @RequestBody SensorDataDTO sensorDataDTO) {
         SensorDataDTO updatedSensorData = sensorDataService.updateSensorData(id, sensorDataDTO);
         return (updatedSensorData != null) ? ResponseEntity.ok(updatedSensorData) : ResponseEntity.notFound().build();
     }
 
+    // Eliminar un registro de sensor
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteSensorData(@PathVariable Long id) {
         sensorDataService.deleteSensorData(id);
