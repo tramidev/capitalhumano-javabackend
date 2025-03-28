4d073f6c8722bef462f2721be832e1acc7dfefda
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2025-03-28 01:24:37 -0300

    CRUD User, Role, UserRole

--- iot-api/src/main/java/com/sensormanager/iot/controller/SensorDataController.java
+++ iot-api/src/main/java/com/sensormanager/iot/controller/SensorDataController.java
@@ -1,20 +1,15 @@
 package com.sensormanager.iot.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sensormanager.iot.dto.SensorDataDTO;
 import com.sensormanager.iot.service.SensorDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sensordata")
