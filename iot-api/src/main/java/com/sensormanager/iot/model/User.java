38024b5a2518f72746dc111324d8198fdfedd616
Cristian Aliaga <132721848+cristian-aliaga@users.noreply.github.com>
2025-04-14 11:11:16 -0400

    Modification of User GenerativeValue & Deleted Unused Imports

--- iot-api/src/main/java/com/sensormanager/iot/model/User.java
+++ iot-api/src/main/java/com/sensormanager/iot/model/User.java
@@ -5,7 +5,20 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,17 +35,22 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_sequence", allocationSize = 1, initialValue = 6)
+    @Column(unique = true)
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String username;
     private String password;
+    @Column(unique = true)
     private String userEmail;
     private Long userCreatedAt;
     private Long userExpireAt;
 
     @Column(name = "user_status", nullable = false)
+    @Builder.Default
     private Boolean userStatus = true;
 
     @ManyToOne(fetch = FetchType.EAGER)
@@ -52,7 +70,7 @@ public class User {
         this.userCreatedAt = Instant.now().getEpochSecond();
         this.userExpireAt = Instant.now().plus(365, ChronoUnit.DAYS).getEpochSecond();
     }
-    
+
     public boolean hasRole(String role) {
         for (Role authority : this.getRoleName()) {
             if (authority.getRoleName().equals(role)) {
